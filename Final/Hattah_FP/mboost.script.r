d57dff18f5a94368c66bdefee1ff096741b780db
Cassie James <cassiesjames@gmail.com>
2016-08-16 12:00:46 +1000

    mboost richness model falls over

--- Final/Hattah_FP/mboost.script.r
+++ Final/Hattah_FP/mboost.script.r
@@ -41,7 +41,7 @@ envdata$Flood_frequency=as.numeric(scale(envdata$Flood_frequency,center=TRUE, sc
 envdata$TSLF=as.numeric(scale(log10(envdata$TSLF+1),center=TRUE, scale=FALSE))
 envdata$Easting=as.numeric(scale(envdata$Easting^2,center=FALSE, scale=TRUE))
 envdata$Northing=as.numeric(scale(envdata$Northing^2,center=FALSE, scale=TRUE))
-chart.Correlation(envdata, histogram=TRUE, pch=19)
+
 
 envdata$INT <- rep(1,nrow(envdata)) # provide intercept variable
 
@@ -97,6 +97,8 @@ mFf<-mean(envdata_backup$Flood_frequency)
 
 xmatLin <- extract(Full.model, which=8)
 xmatSmooth <- extract(Full.model, which=9)
+
+par(mfrow = c(3,2))
 
 # the below line had to adapted from the paper to correct for the type of speech marks, the order or the components and the spacing around the equals signs: see coef(Full.model)
 yvalues=xmatSmooth[[1]]%*%coef(Full.model)$`bbs(Flood_frequency, df = 1, center = TRUE)` + xmatLin[[1]]*coef(Full.model)$`bols(Flood_frequency, intercept = FALSE)`
@@ -161,7 +163,8 @@ r2<-(1-exp(-2/n*(sum(L1)-sum(L0))))/(1-exp(sum(L0))^{2/n})
 n<- nrow(daten.rich)
 set.seed(806)
 indvecL <-sample(1:n,n,replace=FALSE) # create a random set of numbers
-datenSmallrich <- daten.rich[indvecL,][1:(n/2),] # create a subset of half the original data using the random numbers
+datenSmall.rich <- daten.rich[indvecL,][1:(n/2),] # create a subset of half the original data using the random numbers
+datenSmall.rich$Inundated.y=as.factor(datenSmall.rich$Inundated.y)
 
 #specify model formula - kept relatively simple of the time being (bols are linear effects, bbs are smoothed effects and bspatial are spatial effects)
 formulaB <- Richness ~ bols(Easting, intercept=FALSE)+bols(Northing, intercept=FALSE)+
@@ -181,89 +184,18 @@ null.model.nu<-list()
 # specify training set
 
 set.seed(1)
-n<-nrow(datenSmallrich)
+n<-nrow(datenSmall.rich)
 indvec<-sample(1:n,n,replace=TRUE)
-traindatarich<-datenSmallrich[indvec,]
-traindatav2rich=traindatarich[,-1] # remove site names from data frame
+traindata.rich<-datenSmall.rich[indvec,]
+traindatav2.rich=traindata.rich[,-1] # remove site names from training data set
 
-# Run model
+# Run model - FAILS
 
-Full.model.rich <-gamboost(formulaB,data=traindatav2rich, family=Gaussian(), control=boost_control(mstop=1000,trace=TRUE)) # original paper has mstop= 10000 but I reduced down the mstop value for trial runs as it takes a while
-# This churns out a couple of warnings that I don't fully understand regarding the linear effects - "covariates should be (mean-) centered if intercept =False for Easting, Northing and d30"
+Full.model.rich <-gamboost(formulaB,data=traindatav2.rich, family=Poisson(), control=boost_control(mstop=2000,trace=TRUE)) # 
+# This model FAILS with an error around singularity which I think means that there are responses that are NOT unique combinations of the predictors - this did not happen for the diversity index
+# because this index takes into count abundance as well as p/a
 mopt <- mstop(aic <- AIC(Full.model.rich)) 
 
-# Carry out 5 fold cross validation to determine optimal stopping iteration  - increase cross validation for proper model runs?
-cv5f <- cv(model.weights(Full.model.rich), type='kfold', B=5)
-cvm <- cvrisk(Full.model.rich, folds=cv5f)
-plot(cvm)
-st<-(mstop(cvm))
-Full.model.rich[st]
-
-# Example of marginal functional estimates of boosted additive models for flood frequency, time since last flood and rainfall in  days prior to sampling
-# rem that data has been centred so when looking at the plots its helpful to 'uncentre' the data
-mFf<-mean(envdata_backup$Flood_frequency)
-
-xmatLin <- extract(Full.model, which=8)
-xmatSmooth <- extract(Full.model, which=9)
-
-# the below line had to adapted from the paper to correct for the type of quotation marks, the order of the components and the spacing around the equals signs: see coef(Full.model) 
-yvalues=xmatSmooth[[1]]%*%coef(Full.model)$`bbs(Flood_frequency, df = 1, center = TRUE)` + xmatLin[[1]]*coef(Full.model)$`bols(Flood_frequency, intercept = FALSE)`
-plot(sort(traindata$Flood_frequency+mFf),yvalues[order(traindata$Flood_frequency+mFf)], type="l",xlab='Flood frequency', ylab='f(Flood frequency)')
-rug(sort(traindata$Flood_frequency+mFf))
-
-#  plot using Time Since Last Flood (TSLF)
-mTSLF<-mean(log10(envdata_backup$TSLF+1))
-xmatSmooth <- extract(Full.model, which=11)
-# the below line had to adapted from the paper to correct for the type of speech marks, the order and the spacing around the equals signs: see coef(Full.model)
-yvalues=xmatSmooth[[1]]%*%coef(Full.model)$`bbs(TSLF, df = 1, center = TRUE)`
-plot(sort(traindata$TSLF+mTSLF),yvalues[order(traindata$TSLF+mTSLF)], type="l",xlab='TSLF', ylab='f(log(TSLF+1))')
-rug(sort(traindata$TSLF+mTSLF))
-
-# plot using d365(accumulated rainfall in 90 days prior to sampling)
-md365<-mean(log10(envdata_backup$d365+1))
-xmatSmooth <- extract(Full.model, which=7)
-# the below line had to adapted from the paper to correct for the type of speech marks, the order and the spacing around the equals signs: see coef(Full.model)
-yvalues=xmatSmooth[[1]]%*%coef(Full.model)$`bbs(d365, df = 1, center = TRUE)` 
-plot(sort(traindata$d365+md365),yvalues[order(traindata$d365+md365)], type="l",xlab='d365', ylab='f(log(d365+1))')
-rug(sort(traindata$d365+md365))
-
-# Predictions for out-of-bootstrap data
-
-testdata <- datenSmall[-indvec,]
-predictions<-predict(Full.model,newdata=testdata)
-plot(exp(predictions),testdata$H_index)
-abline(1,0)
-
-# Compute pseudo r^2
-
-m1 <-glm(H_index~1,data=traindata, family=gaussian)
-null.model.coef <- coef(m1)
-
-y<-testdata$H_indexlamba<-exp(predictions)
-lambda<-exp(predictions)
-
-L1<-y*log(lambda)-lgamma(y+1)-lambda
-c0<-exp(null.model.coef)
-L0<-y*log(c0)-lgamma(y+1)-c0
-
-n<-length(y)
-r2<-(1-exp(-2/n*(sum(L1)-sum(L0))))/(1-exp(sum(L0))^{2/n})
-
-
-# Compute out-of-bootstrap r^2 - NOT SURE I have done this correctly - I want to measure the predictive accuracy but I am not sure what I am doing here!
-
-m1 <-glm(H_index~1,data=testdata, family=gaussian)
-null.model.coef <- coef(m1)
-
-y<-testdata$H_indexlamba<-exp(predictions)
-lambda<-exp(predictions)
-
-L1<-y*log(lambda)-lgamma(y+1)-lambda
-c0<-exp(null.model.coef)
-L0<-y*log(c0)-lgamma(y+1)-c0
-
-n<-length(y)
-r2<-(1-exp(-2/n*(sum(L1)-sum(L0))))/(1-exp(sum(L0))^{2/n})
 
 
 #########################Further plotting of model and partial effects
